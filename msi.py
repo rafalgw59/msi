21de05a09a8317f2b6ce89ca40448e649dfc9eb2
rafalgw59 <r.gaw59lik@gmail.com>
2024-02-05 14:38:09 +0100

    wykresy

--- msi.py
+++ msi.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 #Parametry
 n = 20  # Liczba okresów do przodu (ile do przodu)
-k = 50 # rząd predykcji (z ilu próbke do predykcji)
+#k = 50 # rząd predykcji (z ilu próbke do predykcji)
 
 
 # kolumny
@@ -25,20 +25,21 @@ def create_dataset(dataset, k, n):
         Y.append(dataset[i + k : i + k + n])
     return np.array(X), np.array(Y)
 
-def generate_predictions(model,dataset,k,n,Dmax,Dmin):
+def predictions(model,dataset,k,n,Dmax,Dmin):
+    #predykcja po okienkach
     predictions = []
     for i in range(0, len(dataset) - k - n + 1, n):
         current_window = dataset[i:i + k].reshape(1, k)
         predicted = model.predict(current_window).flatten()
         predictions.extend(predicted)
-
+    #ostatnie okienko - może nie mieć wystarczającej liczby wartości
     last_window_start = len(dataset) - k - n
     last_window = dataset[last_window_start:last_window_start + k].reshape(1, k)
     last_prediction = model.predict(last_window).flatten()
     predictions.extend(last_prediction[:len(dataset) - last_window_start - k])
 
 
-    #sse
+    #obliczanie sse metodą z zajęć
     pred_len = len(predictions)
     delta = dataset[-pred_len:]-predictions
     delta_shape = delta.shape
@@ -51,9 +52,10 @@ def generate_predictions(model,dataset,k,n,Dmax,Dmin):
 
     print(f'SSE metoda z msi: {sse_msi}')
 
+    #denormalizacja do rzeczywistych wartości
     dataset_scaled = (dataset * (Dmax - Dmin)) + Dmin
     predictions_scaled = (np.array(predictions) * (Dmax - Dmin)) + Dmin
-
+    #spłaszczenie do wykresu
     dataset_scaled_flatten = dataset_scaled.flatten()
     predictions_scaled_flatten = predictions_scaled.flatten()
 
@@ -65,6 +67,7 @@ def generate_predictions(model,dataset,k,n,Dmax,Dmin):
     plt.xlabel('Próbki')
     plt.ylabel('Cena')
     plt.legend()
+    plt.savefig(f"./ploty/CDR_predykcja_k_{k}_cały.png")
 #    plt.show()
 
     #przybliżenie (ostatnie 70 próbek)
@@ -81,21 +84,23 @@ def generate_predictions(model,dataset,k,n,Dmax,Dmin):
     plt.xlabel('Próbki')
     plt.ylabel('Cena')
     plt.legend()
-    plt.show()
+    plt.savefig(f"./ploty/CDR_predykcja_k_{k}_ostatnie_{k+n}_wartosci.png")
+   #plt.show()
 
-def prediction(model,dataset,to_predict,k,n,Dmax,Dmin,target_Dmax,target_Dmin):
+def prediction_koszyk(model,dataset,to_predict,k,n,Dmax,Dmin,target_Dmax,target_Dmin):
+    #predykcja po okienkach
     predictions = []
     for i in range(0, len(dataset) - k - n + 1, n):
         current_window = to_predict[i:i + k].reshape(1, k)
         predicted = model.predict(current_window).flatten()
         predictions.extend(predicted)
-
+    #ostatnie okienko - może nie mieć wystarczającej liczby wartości
     last_window_start = len(dataset) - k - n
     last_window = to_predict[last_window_start:last_window_start + k].reshape(1, k)
     last_prediction = model.predict(last_window).flatten()
     predictions.extend(last_prediction[:len(dataset) - last_window_start - k])
 
-    # sse
+    # sse metodą z zajęć
     pred_len = len(predictions)
     delta = dataset[-pred_len:] - predictions
     delta_shape = delta.shape
@@ -108,10 +113,11 @@ def prediction(model,dataset,to_predict,k,n,Dmax,Dmin,target_Dmax,target_Dmin):
 
     print(f'SSE metoda z msi: {sse_msi}')
 
+    #denormalizacja - do rzeczywistych wartości
     dataset_scaled = (dataset * (Dmax - Dmin)) + Dmin
     to_predict_scaled = (to_predict * (target_Dmax-target_Dmin)) + target_Dmin
     predictions_scaled = (np.array(predictions) * (target_Dmax-target_Dmin)) + target_Dmin
-
+    #spłaszczenie do wykresu
     dataset_scaled_flatten = dataset_scaled.flatten()
     to_predict_scaled_flatten = to_predict_scaled.flatten()
     predictions_scaled_flatten = predictions_scaled.flatten()
@@ -126,7 +132,8 @@ def prediction(model,dataset,to_predict,k,n,Dmax,Dmin,target_Dmax,target_Dmin):
     plt.xlabel('Próbki')
     plt.ylabel('Cena')
     plt.legend()
-    plt.show()
+    #plt.show()
+    plt.savefig(f"./ploty/CDR_predykcja_koszyk_k_{k}_cały.png")
 
     # przybliżenie (ostatnie 70 próbek)
     total_samples = k + n
@@ -143,14 +150,16 @@ def prediction(model,dataset,to_predict,k,n,Dmax,Dmin,target_Dmax,target_Dmin):
     plt.xlabel('Próbki')
     plt.ylabel('Cena')
     plt.legend()
-    plt.show()
+    #plt.show()
+    plt.savefig(f"./ploty/CDR_predykcja_koszyk_k_{k}_ostatnie_{n+k}_wartosci.png")
 
-def a(l_neuro1,l_neuro2):
+def a(l_neuro1,l_neuro2,k):
+    #wczytywanie danych z csv
     df = pd.read_csv('cdr_d.csv', names=colnames)
-    data = df.tail(1000)
-    data = data.drop(['najwyzszy', 'najnizszy', 'zamkniecie', 'wolumen'], axis=1)
+    data = df.tail(1000) #ostatnie 1000 wartości
+    data = data.drop(['najwyzszy', 'najnizszy', 'zamkniecie', 'wolumen'], axis=1) #usuwanie niepotrzebnych kolumn
 
-    data["otwarcie"] = pd.to_numeric(data["otwarcie"], errors="coerce")
+    data["otwarcie"] = pd.to_numeric(data["otwarcie"], errors="coerce") #wszystkie wartości numeryczne
 
     # normalizacja
     Dmax = data["otwarcie"].max()
@@ -159,37 +168,45 @@ def a(l_neuro1,l_neuro2):
 
     dataset = data["otwarcie"].to_numpy()
 
-    X, Y = create_dataset(dataset, k, n)
-    print("X:", X)
-    print("Y:", Y)
+    X, Y = create_dataset(dataset, k, n) #okienka
+    # print("X:", X)
+    # print("Y:", Y)
 
     #sieć neuronowa
+
+    #split
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
+    #model
     model = Sequential()
     model.add(Dense(l_neuro1, activation='relu', input_shape=(k,))) #warstwa 1 liczba wejsc
     model.add(Dense(l_neuro2, activation='relu')) #wartwa 2
     model.add(Dense(n)) #liczba wyjść
 
+    #trenowanie, wagi obliczane w procesie uczenia, optymalizator 'adam'
     model.compile(optimizer='adam', loss='mean_squared_error')
-    history = model.fit(X_train, Y_train, epochs=100, batch_size=32, validation_data=(X_test, Y_test), verbose=1)
+    model.fit(X_train, Y_train, epochs=100, batch_size=32, validation_data=(X_test, Y_test), verbose=1)
 
+    #spradzenie jakości
     test_loss = model.evaluate(X_test, Y_test)
     print(f'Test Loss: {test_loss}')
-    generate_predictions(model,dataset,k,n,Dmax,Dmin)
-    #generate_last_n(model,dataset,n,k,Dmax,Dmin)
+    predictions(model, dataset, k, n, Dmax, Dmin)
 
 
 
-def b(l_neuro1, l_neuro2):
+
+def b(l_neuro1, l_neuro2,k):
+    #nazwy plików z danymi do koszyka
     filenames = ['cdr_d.csv', '3rg_d.csv', '11b_d.csv', 'alg_d.csv', 'art_d.csv', 'bbt_d.csv']
+    #nazwy spółek
     comp_names = ['CD-Project', '3R Games', '11 bit studios', 'All in! Games', 'Artifex Mundi', 'BoomBit']
 
+    #wczytywanie wartości
     aggregated_data = pd.DataFrame()
 
     for i, filename in enumerate(filenames):
         df = pd.read_csv(filename, names=colnames)
-        df = df.tail(1000)  # Wybieramy ostatnie 1000 rekordów
+        df = df.tail(1000)  # ostatnie 1000 wartości
         df = df.drop(['najwyzszy', 'najnizszy', 'zamkniecie', 'wolumen'], axis=1)
         df["otwarcie"] = pd.to_numeric(df["otwarcie"], errors="coerce")  # Konwersja na numeryczny typ danych
 
@@ -230,7 +247,7 @@ def b(l_neuro1, l_neuro2):
     X, Y = create_dataset(dataset, k, n)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-    # Budowa i trening modelu
+    # Budowa i trening modelu, wagi obliczane w procesie uczenia, optymalizator 'adam'
     model = Sequential()
     model.add(Dense(l_neuro1, activation='relu', input_shape=(k,)))
     model.add(Dense(l_neuro2, activation='relu'))
@@ -241,68 +258,27 @@ def b(l_neuro1, l_neuro2):
     test_loss = model.evaluate(X_test, Y_test)
     print(f'Test Loss: {test_loss}')
 
-    # Przewidywania
-    cdr_values_to_predict = cdr_dataset[-k:].reshape(1, k)
-    cdr_real_values = cdr_dataset[-n:].reshape(1,n)
-
-    koszyk_values = dataset[-k:].reshape(1, k)
-    predicted_prices = model.predict(cdr_values_to_predict)
-
-    # sse
-    pred_len = len(predicted_prices)
-    delta = cdr_real_values[-pred_len:] - predicted_prices
-    delta_shape = delta.shape
-    delta = delta.flatten()
-    suma = 0
-    sse_msi = 0
-    for i in range(delta_shape[0]):
-        suma = suma + delta[i] * delta[i]
-    sse_msi = suma / delta_shape[0]
-
-    print(f'SSE metoda z msi: {sse_msi}')
-    prediction(model,dataset,cdr_dataset,k,n,Dmax,Dmin,cdr_Dmax,cdr_Dmin)
-    # #denormalizacja
-    # cdr_values_to_predict = cdr_values_to_predict * (cdr_Dmax - cdr_Dmin) + cdr_Dmin
-    # cdr_real_values = cdr_real_values * (cdr_Dmax - cdr_Dmin) + cdr_Dmin
-    # koszyk_values = koszyk_values * (Dmax - Dmin) + Dmin
-    # predicted_prices = predicted_prices * (Dmax - Dmin) + Dmin
-    #
-    #
-    # cdr_values_flat = cdr_values_to_predict.flatten()
-    # cdr_real_values_flat = cdr_real_values.flatten()
-    # koszyk_values_flat = koszyk_values.flatten()
-    # predicted_prices_flat = predicted_prices.flatten()
-    #
-    # #wykres
-    # plt.figure(figsize=(15,6))
-    # plt.plot(dataset.flatten(), label='Średnie wartości koszyka', color='blue')
-    # plt.plot(cdr_dataset.flatten(), label='Wartości CD-Project', color='green')
-    # plt.plot()
-    #
-    #
-    # # Wykres przyblizenie (70 wartosci)
-    # time_sequence = np.arange(len(koszyk_values_flat) + len(predicted_prices_flat))
-    # combined_sequence = np.concatenate((koszyk_values_flat, predicted_prices_flat))
-    #
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(time_sequence[:len(koszyk_values_flat)], koszyk_values_flat, 'b-', label=f'Ostatnie {k} średnich wartości koszyka')
-    # plt.plot(time_sequence[len(koszyk_values_flat):], predicted_prices_flat, 'r--', label=f'Następne {n} wartości CD-Project (predykcja)')
-    # plt.plot(time_sequence[len(koszyk_values_flat):], cdr_real_values_flat, 'g--', label=f'Następne {n} wartości CD-Project (rzeczywiste)')
-    #
-    # plt.title('Przewidywana średnia cena akcji')
-    # plt.xlabel('Czas')
-    # plt.ylabel('Cena')
-    # plt.legend()
-    # plt.show()
+    #predykcja wartości cdr na podstawie koszyka
+    prediction_koszyk(model, dataset, cdr_dataset, k, n, Dmax, Dmin, cdr_Dmax, cdr_Dmin)
 
 
 
-
+# automat
 def start(podpunkt,l_neuro1,l_neuro2):
     if podpunkt == "a":
-        a(l_neuro1,l_neuro2)
+        k_values = [50,25,20,10,100]
+        for k in k_values:
+            a(l_neuro1,l_neuro2,k)
     elif podpunkt == "b":
-        b(l_neuro1,l_neuro2)
+        k_values = [50,25,20,10,100]
+        for k in k_values:
+            b(l_neuro1,l_neuro2,k)
 
 
 start("a",64,32)
+
+def zadania():
+    start("a",64,32)
+    start("b",64,32)
+
+zadania()
